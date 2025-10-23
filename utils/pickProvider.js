9211f32b77897cdf8778979ebc251e31df2dd2af
NinoMaverick <etengyemmy@gmail.com>
2025-10-23 14:33:53 +0100

    Empty main branch for review base

--- utils/pickProvider.js
+++
@@ -1,9 +0,0 @@
-const pickProvider = () => {
-  // Randomly choose between Paystack or Flutterwave
-  const providers = ['Paystack', 'Flutterwave'];
-  const randomProvider = providers[Math.floor(Math.random() * providers.length)];
-  
-  return randomProvider;
-};
-
-module.exports = pickProvider;
